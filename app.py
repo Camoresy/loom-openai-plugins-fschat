26266a20e9d3f24ce43ffcd3f63d7eeb0355b0c6
Camoresy <z2533736852@gmail.com>
2024-02-01 16:33:47 +0800

    异常兼容

--- app.py
+++ app.py
@@ -63,104 +63,96 @@ class FastChatApplicationAdapter(ApplicationAdapter):
         fastchat_process_dict.processes = {"online_api": {}, "model_worker": {}}
         fastchat_process_dict.mp_manager = mp.Manager()
         self.controller_started = fastchat_process_dict.mp_manager.Event()
-        if openai_api:
-            process = Process(
-                target=run_controller,
-                name=f"controller",
-                kwargs=dict(cfg=self._cfg,
-                            log_level=processesInfo.log_level, started_event=self.controller_started),
-                daemon=True,
-            )
-            fastchat_process_dict.processes["controller"] = process
 
-            process = Process(
-                target=run_openai_api,
-                name=f"openai_api",
-                kwargs=dict(cfg=self._cfg,
-                            log_level=processesInfo.log_level, started_event=self.controller_started),
-                daemon=True,
-            )
-            fastchat_process_dict.processes["openai_api"] = process
+        try:
+            if openai_api:
+                process = Process(
+                    target=run_controller,
+                    name=f"controller",
+                    kwargs=dict(cfg=self._cfg,
+                                log_level=processesInfo.log_level, started_event=self.controller_started),
+                    daemon=True,
+                )
+                fastchat_process_dict.processes["controller"] = process
 
-        if model_worker:
-            for model_name in model_names:
-                config = get_model_worker_config(cfg=self._cfg, model_name=model_name)
-                if not config.get("online_api"):
-                    e = fastchat_process_dict.mp_manager.Event()
-                    self.model_worker_started.append(e)
-                    process = Process(
-                        target=run_model_worker,
-                        name=f"model_worker - {model_name}",
-                        kwargs=dict(cfg=self._cfg,
-                                    model_name=model_name,
-                                    controller_address=controller_address,
-                                    log_level=processesInfo.log_level,
-                                    started_event=e),
-                        daemon=True,
-                    )
-                    fastchat_process_dict.processes["model_worker"][model_name] = process
+                process = Process(
+                    target=run_openai_api,
+                    name=f"openai_api",
+                    kwargs=dict(cfg=self._cfg,
+                                log_level=processesInfo.log_level, started_event=self.controller_started),
+                    daemon=True,
+                )
+                fastchat_process_dict.processes["openai_api"] = process
 
-        if api_worker:
-            for model_name in model_names:
-                config = get_model_worker_config(cfg=self._cfg, model_name=model_name)
-                if (config.get("online_api")
-                        and config.get("worker_class")
-                        and model_name in self._cfg.get_cfg().get("fschat_model_workers", [])):
-                    e = None
-                    self.model_worker_started.append(e)
-                    process = Process(
-                        target=run_model_worker,
-                        name=f"api_worker - {model_name}",
-                        kwargs=dict(cfg=self._cfg,
-                                    model_name=model_name,
-                                    controller_address=controller_address,
-                                    log_level=processesInfo.log_level,
-                                    started_event=e),
-                        daemon=True,
-                    )
-                    fastchat_process_dict.processes["online_api"][model_name] = process
+            if model_worker:
+                for model_name in model_names:
+                    config = get_model_worker_config(cfg=self._cfg, model_name=model_name)
+                    if not config.get("online_api"):
+                        e = fastchat_process_dict.mp_manager.Event()
+                        self.model_worker_started.append(e)
+                        process = Process(
+                            target=run_model_worker,
+                            name=f"model_worker - {model_name}",
+                            kwargs=dict(cfg=self._cfg,
+                                        model_name=model_name,
+                                        controller_address=controller_address,
+                                        log_level=processesInfo.log_level,
+                                        started_event=e),
+                            daemon=True,
+                        )
+                        fastchat_process_dict.processes["model_worker"][model_name] = process
+
+            if api_worker:
+                for model_name in model_names:
+                    config = get_model_worker_config(cfg=self._cfg, model_name=model_name)
+                    if (config.get("online_api")
+                            and config.get("worker_class")
+                            and model_name in self._cfg.get_cfg().get("fschat_model_workers", [])):
+                        e = None
+                        self.model_worker_started.append(e)
+                        process = Process(
+                            target=run_model_worker,
+                            name=f"api_worker - {model_name}",
+                            kwargs=dict(cfg=self._cfg,
+                                        model_name=model_name,
+                                        controller_address=controller_address,
+                                        log_level=processesInfo.log_level,
+                                        started_event=e),
+                            daemon=True,
+                        )
+                        fastchat_process_dict.processes["online_api"][model_name] = process
+
+        except Exception as e:
+            logger.error("Failed to init fastchat", exc_info=True)
+            fastchat_process_dict.stop_all()
 
     def start(self):
-        # 保证任务收到SIGINT后，能够正常退出
-        if p := fastchat_process_dict.processes.get("controller"):
-            p.start()
-            p.name = f"{p.name} ({p.pid})"
-            self.controller_started.wait()  # 等待controller启动完成
+        try:
+            # 保证任务收到SIGINT后，能够正常退出
+            if p := fastchat_process_dict.processes.get("controller"):
+                p.start()
+                p.name = f"{p.name} ({p.pid})"
+                self.controller_started.wait()  # 等待controller启动完成
 
-        if p := fastchat_process_dict.processes.get("openai_api"):
-            p.start()
-            p.name = f"{p.name} ({p.pid})"
+            if p := fastchat_process_dict.processes.get("openai_api"):
+                p.start()
+                p.name = f"{p.name} ({p.pid})"
 
-        for n, p in fastchat_process_dict.processes.get("model_worker", {}).items():
-            p.start()
-            p.name = f"{p.name} ({p.pid})"
+            for n, p in fastchat_process_dict.processes.get("model_worker", {}).items():
+                p.start()
+                p.name = f"{p.name} ({p.pid})"
 
-        for n, p in fastchat_process_dict.processes.get("online_api", []).items():
-            p.start()
-            p.name = f"{p.name} ({p.pid})"
+            for n, p in fastchat_process_dict.processes.get("online_api", []).items():
+                p.start()
+                p.name = f"{p.name} ({p.pid})"
 
-        # 等待所有model_worker启动完成
-        for e in self.model_worker_started:
-            e.wait()
+            # 等待所有model_worker启动完成
+            for e in self.model_worker_started:
+                e.wait()
+
+        except Exception as e:
+            logger.error("Failed to start fastchat", exc_info=True)
+            fastchat_process_dict.stop_all()
 
     def stop(self):
-        for p in fastchat_process_dict.processes.values():
-            logger.warning("Sending SIGKILL to %s", p)
-            # Queues and other inter-process communication primitives can break when
-            # process is killed, but we don't care here
-
-            if isinstance(p, dict):
-                for process in p.values():
-                    try:
-
-                        process.kill()
-                    except Exception as e:
-                        logger.info("Failed to kill process %s", p, exc_info=True)
-            else:
-                try:
-                    p.kill()
-                except Exception as e:
-                    logger.info("Failed to kill process %s", p, exc_info=True)
-
-        for p in fastchat_process_dict.processes.values():
-            logger.info("Process status: %s", p)
+        fastchat_process_dict.stop_all()
--- fastchat_process_dict.py
+++ fastchat_process_dict.py
@@ -1,5 +1,30 @@
 from multiprocessing import Process
 from typing import Dict
+import logging
 
+logger = logging.getLogger(__name__)
 mp_manager = None
 processes: Dict[str, Process] = {}
+
+
+def stop_all():
+    for p in processes.values():
+        logger.warning("Sending SIGKILL to %s", p)
+        # Queues and other inter-process communication primitives can break when
+        # process is killed, but we don't care here
+
+        if isinstance(p, dict):
+            for process in p.values():
+                try:
+
+                    process.kill()
+                except Exception as e:
+                    logger.info("Failed to kill process %s", p, exc_info=True)
+        else:
+            try:
+                p.kill()
+            except Exception as e:
+                logger.info("Failed to kill process %s", p, exc_info=True)
+
+    for p in processes.values():
+        logger.info("Process status: %s", p)
